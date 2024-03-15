0e52f0a3db76ec2920a2ee4d15b42062afc3655c
rizwanfree <121812303+rizwanfree@users.noreply.github.com>
2024-03-15 23:36:30 +0500

    seo

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 from . import views
 
 app_name = 'core'
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name='core/robots.txt', content_type='text/plain')),
     path('', views.index, name='index'),
     path('name/', views.searchByName, name='byname'),
     path('state/', views.statePage, name='states'),
